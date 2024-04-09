2d3f765117b683d1b1116907c722c4d5e9c2a0e3
tena <149326626+tenadhayalan@users.noreply.github.com>
2024-04-08 17:47:55 -0700

    updated 20240408

--- Scripts/expected vs actual.R
+++ Scripts/expected vs actual.R
@@ -28,7 +28,19 @@ ggplot(data=todayspH, aes(x=expected_pH,y=pH, group = interaction(expected_pH,di
   labs(x="Expected pH", y= "Measured pH")+
   theme_bw()
 
-  ##temp plots
+##data without acclimation
+noacc <- pHData[-(1:54),]
+noacc$discrete <- cut(noacc$expected_temp, breaks = 2, labels = c("Low", "High"))
+ggplot(data=noacc, aes(x=expected_pH,y=pH, group = interaction(expected_pH,discrete), fill = discrete, color = discrete))+
+  geom_boxplot(alpha=0.5, outlier.alpha=0) + 
+  scale_y_continuous(breaks = seq(7.0, 8.1, by=0.1), limits=c(7.0,8.1))+
+  scale_x_continuous(breaks = seq(7.1, 8.1, by=0.1), limits=c(7.1,8.1))+
+  scale_fill_manual(values=c("Low" = "#0072B2", "High" = "tomato"))+
+  ##geom_jitter(width=0.25, pch=21, color="white", size=2) +
+  scale_color_manual(values=c("Low" = "#0072B2", "High" = "tomato"))+
+  labs(x="Expected pH", y= "Measured pH")+
+  theme_bw()
+##temp plots
 
 #expected vs actual
 pHData$discrete <- cut(pHData$expected_temp, breaks = 2, labels = c("Low", "High"))
