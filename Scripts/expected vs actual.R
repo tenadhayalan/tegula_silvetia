e8543db2a5952326e169368f832c73afdf429cfd
tena <149326626+tenadhayalan@users.noreply.github.com>
2024-04-15 10:08:49 -0700

    Updated 20240414

--- Scripts/expected vs actual.R
+++ Scripts/expected vs actual.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(ggplot2)
 
-pHData <- read.csv("Data/pH_temp/ph_temp_final.csv")
+pHData <- read_csv(here("Data","pH_temp","ph_temp_final.csv"))
 
 ##ph plots
 pHData$discrete <- cut(pHData$expected_temp, breaks = 2, labels = c("Low", "High"))
@@ -10,20 +10,18 @@ ggplot(data=pHData, aes(x=expected_pH,y=pH, group = interaction(expected_pH,disc
   scale_y_continuous(breaks = seq(7.0, 8.1, by=0.1), limits=c(7.0,8.1))+
   scale_x_continuous(breaks = seq(7.1, 8.1, by=0.1), limits=c(7.1,8.1))+
   scale_fill_manual(values=c("Low" = "#0072B2", "High" = "tomato"))+
-  ##geom_jitter(width=0.25, pch=21, color="white", size=2) +
   scale_color_manual(values=c("Low" = "#0072B2", "High" = "tomato"))+
   labs(x="Expected pH", y= "Measured pH")+
   theme_bw()
 
 ##daily data
-todayspH <- subset(pHData, date %in% c("20240410"))
+todayspH <- subset(pHData, date %in% c("20240414"))
 todayspH$discrete <- cut(todayspH$expected_temp, breaks = 2, labels = c("Low", "High"))
 ggplot(data=todayspH, aes(x=expected_pH,y=pH, group = interaction(expected_pH,discrete), fill = discrete, color = discrete))+
   geom_boxplot(alpha=0.5, outlier.alpha=0) + 
   scale_y_continuous(breaks = seq(7.0, 8.1, by=0.1), limits=c(7.0,8.1))+
   scale_x_continuous(breaks = seq(7.1, 8.1, by=0.1), limits=c(7.1,8.1))+
   scale_fill_manual(values=c("Low" = "#0072B2", "High" = "tomato"))+
-  ##geom_jitter(width=0.25, pch=21, color="white", size=2) +
   scale_color_manual(values=c("Low" = "#0072B2", "High" = "tomato"))+
   labs(x="Expected pH", y= "Measured pH")+
   theme_bw()
@@ -36,7 +34,6 @@ ggplot(data=noacc, aes(x=expected_pH,y=pH, group = interaction(expected_pH,discr
   scale_y_continuous(breaks = seq(7.0, 8.1, by=0.1), limits=c(7.0,8.1))+
   scale_x_continuous(breaks = seq(7.1, 8.1, by=0.1), limits=c(7.1,8.1))+
   scale_fill_manual(values=c("Low" = "#0072B2", "High" = "tomato"))+
-  ##geom_jitter(width=0.25, pch=21, color="white", size=2) +
   scale_color_manual(values=c("Low" = "#0072B2", "High" = "tomato"))+
   labs(x="Expected pH", y= "Measured pH")+
   theme_bw()
@@ -54,7 +51,6 @@ ggplot(data=pHData, aes(x=expected_temp,y=TempInSitu, group=expected_temp, fill=
 #with pH included
 ggplot(data=na.omit(pHData), aes(x=pH,y=TempInSitu, group = interaction(expected_temp,pH), fill = pH, color = pH))+
   geom_boxplot(alpha=0.5, outlier.alpha=0) + 
-  ##geom_jitter(width=0.25, pch=21, size=2) +
   labs(x="pH", y= "Measured temp")+
   theme_bw()
 
@@ -65,3 +61,4 @@ ggplot(data=pHData, aes(x=date,y=TempInSitu, group=expected_temp, fill=discrete)
   geom_jitter(width=0.25, pch=21, size=2) +
   labs(x="Date", y= "Temperature")+
   theme_bw()
+
--- Scripts/predation_code.R
+++ Scripts/predation_code.R
@@ -1,22 +1,29 @@
+# for rockweed predation 
+# by: tena 
+# updated: 04/11/2024
+
 library(tidyverse)
-predation <- read.csv("Data/growth/weight_diff.csv")
+library(here)
+library(janitor)
 
-##by temperature
-ggplot(data=predation, aes(x=temperature,y=weight, group = interaction(predator,temperature), fill = predator, color = predator))+
-  geom_boxplot(alpha=0.5, outlier.alpha=0) + 
-  labs(x="Temperature", y= "Difference in rockweed weight (g)")+
-  theme_bw()
+predation <- read_csv(here("Data","growth","rockweed_weights.csv"))
 
-##by pH
-ggplot(data=predation, aes(x=pH,y=weight, group = interaction(predator,pH), fill = predator, color = predator))+
-  geom_boxplot(alpha=0.5, outlier.alpha=0) + 
-  scale_x_continuous(breaks = seq(7.1, 8.1, by=0.1), limits=c(7.1,8.1))+
-  labs(x="pH", y= "Difference in rockweed weight (g)")+
-  theme_bw()
+#calculating percent change
+predation_wide <- predation %>%
+  pivot_wider(values_from = weight_g, names_from=date) %>%
+  clean_names() %>%
+  mutate(percent_change=100*(x20240410-x20240403)/x20240403) 
 
-##by temperature and pH
-ggplot(data=predation, aes(x=tank,y=weight, group = interaction(predator,tank), fill = predator, color = predator))+
-  geom_boxplot(alpha=0.5, outlier.alpha=0) + 
-  ##scale_x_continuous(breaks = seq(7.1, 8.1, by=0.1), limits=c(7.1,8.1))+
-  labs(x="tank", y= "Difference in rockweed weight (g)")+
-  theme_bw()
+#plot by predation
+predation_wide %>%
+  ggplot(aes(x=factor(p_h),y=percent_change, color=factor(temperature)))+
+  geom_boxplot()+
+  geom_jitter(position=position_dodge())+
+  facet_wrap(~predator)
+
+#plot by temperature
+predation_wide %>%
+  ggplot(aes(x=factor(p_h),y=percent_change, color=predator))+
+  geom_boxplot()+
+  geom_jitter(position=position_dodge())+
+  facet_wrap(~temperature)
